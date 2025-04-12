84f2594495180d61baef0ba263a3b24245e4fd52
hfarzali <huseyn.farzali@pashabank.az>
2025-04-12 11:41:49 +0400

    last touches before the chart-data returning logic are implemented

--- db.go
+++ db.go
@@ -3,6 +3,7 @@ package main
 import (
 	"database/sql"
 	"fmt"
+	_ "github.com/lib/pq"
 	"log"
 	"os"
 	"strings"
@@ -20,7 +21,7 @@ func SetupPostgres() *sql.DB {
 	host := os.Getenv("DB_HOST")
 	port := os.Getenv("DB_PORT")
 	user := os.Getenv("DB_USER")
-	password := os.Getenv("DB_PASSWORD")
+	password := os.Getenv("DB_PASS")
 	dbname := os.Getenv("DB_NAME")
 
 	conn := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
